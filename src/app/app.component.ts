f8bc5094891a032319db561041ed517486b025a0
Qamir alii <a.bitpins@gmail.com>
2024-12-25 14:28:56 +0330

    use aouter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { BodyComponent } from "./body/body.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, BodyComponent],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:BodyComponent},
+    {},
+    {},
+    {}
+];
